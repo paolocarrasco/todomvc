bf2f6b69c8d56ccba93092a2ea9426f52336b8ba
Paolo Carrasco <paolo.carrasco>
2015-11-24 23:49:30 -0500

    eslint is a grunt task

--- examples/vanillajs/gruntfile.js
+++ examples/vanillajs/gruntfile.js
@@ -1,14 +1,18 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-eslint');
 
   grunt.initConfig({
     jasmine: {
-      src: 'js/**/*.js',
+      src: ['js/controller.js'],
       options: {
         specs: [
           'test/**/*Spec.js'
         ]
       }
+    },
+    eslint: {
+        target: ['js/*.js']
     }
   });
 };
