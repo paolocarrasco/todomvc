8ca655bbecfd954b4c4d9d37d6eb9ed5070a5dd2
Paolo Carrasco <paolo.carrasco>
2015-11-26 22:07:52 -0500

    lint of the javascript files is done with jshint

--- examples/vanillajs/gulpfile.js
+++ examples/vanillajs/gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine-browser');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-  gulp
+gulp.task('lint', function() {
+  return gulp
+    .src(['js/*.js', 'test/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+  return gulp
     .src(['js/controller.js', 'test/*Spec.js'])
     .pipe(jasmine.specRunner({console: true}))
     .pipe(jasmine.headless());
